cc53039919d7c58b8dd6966e92e97dfc95aae699
KimelirR <kimeliryans@gmail.com>
2023-04-27 17:41:25 +0300

    second commit

--- cron.php
+++ cron.php
@@ -1,6 +1,7 @@
 <?php 
 
 require_once('supporters.php');
-require_once('cancelledSupporters.php');
 require_once('contributions.php');
 require_once('recurringsupporters.php');
+require_once('cancelledSupporters.php');
+
